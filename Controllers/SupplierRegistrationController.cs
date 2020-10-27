156239f848671f9cf9807e0c02d41143e8220744
George varghese <georgevarghese3@rediffmail.com>
2020-10-27 08:22:20 +0530

    Supplier Registration

---
+++ Controllers/SupplierRegistrationController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WrokFlowWeb.ViewModel.SupplierRegistration;
+using static WrokFlowWeb.Constants.Constants;
+
+namespace WrokFlowWeb.Controllers
+{
+    public class SupplierRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            SupplierRegistration model = new SupplierRegistration
+            {
+                Questions = new List<QuestionModel>()
+            {
+                new QuestionModel()
+                {
+                    QuestionText = "Have you worked with us before",
+                    QuestionControlType = Controls.Textbox,
+                    QuestionComment = "Comment 1",
+                    QuestionAnswer="1"
+                },
+                new QuestionModel()
+                {
+                    QuestionText = "Please provide your details",
+                     QuestionControlType = Controls.Textbox,
+                    QuestionComment = "Comment 2",
+                     QuestionAnswer="10"
+                },
+                 new QuestionModel()
+                {
+                    QuestionText = "Have you worked with us before on which time",
+                     QuestionControlType = Controls.MultiLineTextox,
+                    QuestionComment = "Comment 2",
+                     QuestionAnswer="10"
+                }, new QuestionModel()
+                {
+                    QuestionText = "Please select the country",
+                    QuestionControlType = Controls.Dropdown,
+                    QuestionComment = "Comment 2",
+                    QuestionAnswer="10",
+                    SelectedValue = 1001,
+                    DataSource = new List<DropdownSource>()
+                    {
+                        new DropdownSource()
+                        {
+                            Id = 0,
+                            Text = ""
+                        },
+                        new DropdownSource()
+                        {
+                            Id = 1000,
+                            Text = "India"
+                        },
+                         new DropdownSource()
+                        {
+                            Id = 1001,
+                            Text = "USA"
+                        },
+                          new DropdownSource()
+                        {
+                            Id = 1002,
+                            Text = "Canada"
+                        }
+                    }
+                },
+                  new QuestionModel()
+                {
+                    QuestionText = "Please explain your proficency",
+                    QuestionControlType = Controls.Textbox,
+                    QuestionComment = "Comment 1",
+                    QuestionAnswer=""
+                },
+            }
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(SupplierRegistration supplierRegistration)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ ViewModel/SupplierRegistration/SupplierRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static WrokFlowWeb.Constants.Constants;
+
+namespace WrokFlowWeb.ViewModel.SupplierRegistration
+{
+    public class SupplierRegistration
+    {
+        public List<QuestionModel> Questions { get; set; }
+    }
+
+    public class QuestionModel
+    {
+        public string QuestionText { get; set; }
+        public string QuestionComment { get; set; }
+        public Controls QuestionControlType { get; set; }
+        public string QuestionAnswer { get; set; }
+        public List<DropdownSource> DataSource { get; set; }
+        public long SelectedValue { get; set; }
+    }
+    public class AnswerModel
+    {
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+
+    public class DropdownSource
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+    }
+}
